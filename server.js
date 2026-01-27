7e6fcedb7449fd7b8e867b27df868ba83b170f11
William Matton <williammatton@gmail.com>
2026-01-27 15:35:26 +0100

    Virrvarr av ändringar.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import richardsAPI from './movieAPI.js';
 
 const server = express();
+
+server.get('/movies', async(req, res) => {
+	const movies = await richardsAPI.getMovies();
+	res.send(movies);
+});
 
 server.use('/static', express.static('./static'));
 
