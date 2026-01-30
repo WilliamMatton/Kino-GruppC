bbac912e82314ff93c4e6bd18a17e9a23f2ea2ac
Joseph <joseph.lindetun@gmail.com>
2026-01-30 11:55:48 +0100

    Vi fixade med strukturen för api:et
    Co-authored-by: WilliamMatton <WilliamMatton@users.noreply.github.com>
    Co-authored-by: Mattias Strandberg <mastr777@users.noreply.github.com>

--- movieAPI.js
+++ movieAPI.js
@@ -1,4 +1,6 @@
 const MOVIE_API = 'https://plankton-app-xhkom.ondigitalocean.app/api';
+const REVIEWS_API = 'https://plankton-app-xhkom.ondigitalocean.app/api/reviews';
+const SCREENINGS_API = 'https://plankton-app-xhkom.ondigitalocean.app/api/screenings';
 
 async function getMovies() {
   const res = await fetch(MOVIE_API + '/movies');
@@ -12,9 +14,37 @@ async function getMovie(id) {
   return json.data;
 }
 
+async function getReviews() {
+  const res = await fetch(REVIEWS_API + '/reviews');
+  const text = await res.json();
+  return text.data;
+  
+}
+
+async function getReviewrating(rating){
+  const res = await fetch(REVIEWS_API + '/reviews/' + rating);
+  const json = await res.json();
+  return json.data;
+}
+
+async function getUpcomingScreeningsForMovie(movieId) {
+  const now = new Date().toISOString();
+
+  const url =
+    MOVIE_API +
+    `/screenings?filters[movie]=${movieId}&filters[start_time][$gte]=${encodeURIComponent(now)}&sort=start_time:asc`;
+
+  const res = await fetch(url);
+  const json = await res.json();
+  return json.data;
+}
+
 const richardsAPI = {
   getMovies,
-  getMovie
+  getMovie,
+  getReviewrating,
+  getReviews,
+  getUpcomingScreeningsForMovie,
 }
 
 export default richardsAPI;
